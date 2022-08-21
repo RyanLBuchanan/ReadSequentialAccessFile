d8cfc72f3de14cd1ba37d427d40cdbf618632048
Ryan Lowell Buchanan <buchanan.ryan22@gmail.com>
2022-08-21 08:46:11 -0600

    Add project files.

---
+++ ReadSequentialAccessFile/ReadSequentialAccessFileForm.Designer.cs
@@ -0,0 +1,77 @@
+﻿namespace ReadSequentialAccessFile
+{
+    partial class ReadSequentialAccessFileForm
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.nextButton = new System.Windows.Forms.Button();
+            this.openButton = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // nextButton
+            // 
+            this.nextButton.Location = new System.Drawing.Point(400, 400);
+            this.nextButton.Name = "nextButton";
+            this.nextButton.Size = new System.Drawing.Size(140, 32);
+            this.nextButton.TabIndex = 11;
+            this.nextButton.Text = "Next Record";
+            this.nextButton.UseVisualStyleBackColor = true;
+            this.nextButton.Click += new System.EventHandler(this.nextButton_Click);
+            // 
+            // openButton
+            // 
+            this.openButton.Location = new System.Drawing.Point(240, 400);
+            this.openButton.Name = "openButton";
+            this.openButton.Size = new System.Drawing.Size(140, 32);
+            this.openButton.TabIndex = 10;
+            this.openButton.Text = "Open File";
+            this.openButton.UseVisualStyleBackColor = true;
+            this.openButton.Click += new System.EventHandler(this.openButton_Click);
+            // 
+            // ReadSequentialAccessFileForm
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(11F, 25F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(587, 461);
+            this.Controls.Add(this.nextButton);
+            this.Controls.Add(this.openButton);
+            this.Name = "ReadSequentialAccessFileForm";
+            this.Text = "Form1";
+            this.Controls.SetChildIndex(this.openButton, 0);
+            this.Controls.SetChildIndex(this.nextButton, 0);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button nextButton;
+        private System.Windows.Forms.Button openButton;
+    }
+}
+
---
+++ ReadSequentialAccessFile/ReadSequentialAccessFileForm.cs
@@ -0,0 +1,103 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.IO;
+using BankLibrary;
+
+namespace ReadSequentialAccessFile
+{
+    public partial class ReadSequentialAccessFileForm : BankUIForm
+    {
+        private StreamReader fileReader;  // Reads data from a text file
+
+        public ReadSequentialAccessFileForm()
+        {
+            InitializeComponent();
+        }
+
+        private void openButton_Click(object sender, EventArgs e)
+        {
+            // Create and show dialog box enabling user to open file
+            DialogResult result;
+            string fileName;
+
+            using (OpenFileDialog fileChooser = new OpenFileDialog())
+            {
+                result = fileChooser.ShowDialog();
+                fileName = fileChooser.FileName;
+            }
+
+            // Ensure that the user clicked "OK"
+            if (result == DialogResult.OK)
+            {
+                ClearTextBoxes();
+
+                // Show error if user specified invalid form
+                if (string.IsNullOrEmpty(fileName))
+                {
+                    MessageBox.Show("Invalid file name", "Error",
+                        MessageBoxButtons.OK, MessageBoxIcon.Error);
+                }
+                else
+                {
+                    try
+                    {
+                        // Create FileStream to obtain read access to file
+                        FileStream input = new FileStream(fileName, FileMode.Open, FileAccess.Read);
+
+                        // Set file from where data is read
+                        fileReader = new StreamReader(input);
+
+                        openButton.Enabled = false;  // Disable Open File buttone
+                        nextButton.Enabled = true;
+                    }
+                    catch (IOException)
+                    {
+                        MessageBox.Show("Error reading from file.", "File error",
+                            MessageBoxButtons.OK, MessageBoxIcon.Error);
+                    }
+                }
+            }
+        }
+
+        private void nextButton_Click(object sender, EventArgs e)
+        {
+            try
+            {
+                // Get nest record available in the file
+                var inputRecord = fileReader.ReadLine();
+
+                if (inputRecord != null)
+                {
+                    string[] inputFields = inputRecord.Split(',');
+
+                    // Copy string-array values to TextBox values
+                    SetTextBoxValues(inputFields);
+                }
+                else
+                {
+                    // Close StreamReader and underlying file
+                    fileReader.Close();
+                    openButton.Enabled = true;
+                    nextButton.Enabled = false;
+                    ClearTextBoxes();
+
+                    // Notify user if no records in file
+                    MessageBox.Show("No more records in file.", "Error", 
+                        MessageBoxButtons.OK, MessageBoxIcon.Error);
+                }
+            }
+            catch (IOException)
+            {
+                MessageBox.Show("Error reading from file", "Error",
+                    MessageBoxButtons.OK, MessageBoxIcon.Error);
+            }
+        }
+    }
+}
